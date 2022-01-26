4c88edd815962662d38095803f989ea5112f4201
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-01-25 21:47:53 -0500

    added more customer info

--- src/components/Customers/Customer.js
+++ src/components/Customers/Customer.js
@@ -3,11 +3,13 @@ import BorderCard from '../common/BorderCard';
 
 const Customer = (props) => {
 
-  const {name} = props.Customer
+  const {name,address} = props.Customer
+  
 
   return (
     <BorderCard>
       <h2>{name}</h2>
+      <h2>{address}</h2>
       
     </BorderCard>
   )
